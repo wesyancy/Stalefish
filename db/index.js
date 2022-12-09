1975630d2e0aba69f710de22c01f30e790fe655a
Wes Yancy <79019393+wesyancy@users.noreply.github.com>
2022-12-08 19:26:10 -0600

    Database PORT fix attempts

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@ const { Client } = require('pg');
 const { DATABASE_URL } = process.env;
 
 const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "graceShopper",
-    password: "password",
+    user: "stalefish_user",
+    host: "dpg-ce98oqhgp3jtsam8hqh0-a",
+    database: "stalefish",
+    password: "ghNHr5sQHvkXMYAihp7acjS9Ku9JWmAM",
     port: 5432,
     // connectionString: DATABASE_URL,
 })
